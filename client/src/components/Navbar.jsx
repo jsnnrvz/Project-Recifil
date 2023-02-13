33ea14a1d0ffc0420a2b31fd9a8cee8c9c9ba703
dolphinal17 <delfin.ivan.bscs2019@gmail.com>
2023-02-13 21:18:22 +0800

    installed tailwind

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='text-center text-[#B2D33D] text-9xl'>BOOM</h1>
+    </div>
+  )
+}
+
+export default Navbar
